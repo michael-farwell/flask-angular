55e109f2fead2ec53a50656e7a2bf15a030f3c70
Michael Farwell <whitewolf9909@gmail.com>
2017-09-25 12:29:20 -0400

    Small change

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def index():
-    return app.send_static_file('static/index.html')
+    return app.send_static_file('index.html')
 
 
 if __name__ == '__main__':
